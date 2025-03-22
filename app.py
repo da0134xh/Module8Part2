e6b3e6e43b0dd79bdcc5be59f5d830a5e7542701
da0134xh <email@>
2025-03-22 03:57:32 -0500

    Final Commit

--- app.py
+++ app.py
@@ -5,66 +5,70 @@ from datetime import datetime
 app = Flask(__name__)
 
 # Configure SQLite database
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///books.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///movies.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
-# Define Book Model
-class Book(db.Model):
+# Define Movie Model
+class Movie(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(200), nullable=False)
-    author = db.Column(db.String(100))
-    pages = db.Column(db.Integer)
-    completed = db.Column(db.Boolean, default=False)
-    rating = db.Column(db.Integer)  # 1-5 stars
-    started_at = db.Column(db.DateTime, default=datetime.utcnow)
+    year_aired = db.Column(db.Integer)
+    watched = db.Column(db.Boolean, default=False)
+    rating = db.Column(db.Integer, default=1)  # 1-5 stars
 
     def __repr__(self):
-        return f'<Book {self.title} by {self.author}>'
+        return f'<Movie {self.title}>'
 
 # Create database tables
 with app.app_context():
     db.create_all()
 
+#For home page
 @app.route('/')
 def index():
-    books = Book.query.order_by(Book.started_at.desc()).all()
-    return render_template('index.html', books=books)
+    movies = Movie.query.order_by(Movie.title.desc()).all()
+    range_values = range(1, 6) # Ratings from 1 to 5 stars
+    return render_template('index.html', movies=movies, range_values=range_values)
 
+#Adds movies to DB
 @app.route('/add', methods=['POST'])
 def add():
     title = request.form.get('title')
-    author = request.form.get('author')
-    pages = request.form.get('pages')
+    year_aired = request.form.get('year_aired')
+    rating = request.form.get('rating')
     
-    if title and author:  # Basic validation
-        new_book = Book(
+    if title and year_aired and rating:  # Basic validation
+        new_movie = Movie(
             title=title,
-            author=author,
-            pages=int(pages) if pages else None
+            year_aired=int(year_aired),
+            rating=int(rating)
         )
-        db.session.add(new_book)
+        db.session.add(new_movie)
         db.session.commit()
     return redirect(url_for('index'))
 
-@app.route('/toggle/<int:book_id>')
-def toggle(book_id):
-    book = Book.query.get_or_404(book_id)
-    book.completed = not book.completed
+#Updates whether watched or not watched
+@app.route('/toggle/<int:movie_id>')
+def toggle(movie_id):
+    movie = Movie.query.get_or_404(movie_id)
+    movie.watched = not movie.watched
     db.session.commit()
     return redirect(url_for('index'))
 
-@app.route('/rate/<int:book_id>/<int:rating>')
-def rate(book_id, rating):
-    book = Book.query.get_or_404(book_id)
-    book.rating = rating
+#Updates rating
+@app.route('/rate/<int:movie_id>/<int:rating>')
+def rate(movie_id, rating):
+    movie = Movie.query.get_or_404(movie_id)
+    movie.rating = rating
     db.session.commit()
     return redirect(url_for('index'))
 
-@app.route('/delete/<int:book_id>')
-def delete(book_id):
-    book = Book.query.get_or_404(book_id)
-    db.session.delete(book)
+#Deletes movie from list
+@app.route('/delete/<int:movie_id>')
+def delete(movie_id):
+    movie = Movie.query.get_or_404(movie_id)
+    db.session.delete(movie)
     db.session.commit()
     return redirect(url_for('index'))
 
